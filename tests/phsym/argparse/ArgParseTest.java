ccda9555063bf069e6249ca222655fd31ae2bb38
phsym <pierre.henri.symoneaux@gmail.com>
2014-12-01 17:37:01 +0100

    Added base for writing tests

---
+++ tests/phsym/argparse/ArgParseTest.java
@@ -0,0 +1,18 @@
+package phsym.argparse;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ArgParseTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
