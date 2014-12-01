5668a0d6596344b9c066440068407faa661ba956
phsym <pierre.henri.symoneaux@gmail.com>
2014-12-01 18:10:01 +0100

    Updated travis settings

--- tests/phsym/argparse/ArgParseTest.java
+++ tests/phsym/argparse/ArgParseTest.java
@@ -2,17 +2,18 @@ package phsym.argparse;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ArgParseTest {
 
 	@Test
 	public void test() {
-		
+		ArgParse argparse = new ArgParse("Test")
+			.description("Description text")
+			.version("1.0")
+			.defaultHelp()
+			.addDefaultErrorHandler()
+			.epilog("Epilog");
 	}
 
 }
